3fee1ae3f62a0bb7e69821d546f300331ac7e304
Jason Ruan <fr0163@wayne.edu>
2023-03-07 16:09:56 -0500

    Added methods to Deck class

--- src/main/java/com/example/studybuddy/Deck.java
+++ src/main/java/com/example/studybuddy/Deck.java
@@ -1,4 +1,38 @@
 package com.example.studybuddy;
-public class Deck {
 
+import java.util.ArrayList;
+
+public class Deck {
+    private String subject;
+    private ArrayList<Flashcard> deck;
+
+    public Deck() {
+        this.subject = "No name";
+        this.deck = new ArrayList<>();
+    }
+
+    public Deck(String subject, ArrayList<Flashcard> deck) {
+        this.subject = subject;
+        this.deck = deck;
+    }
+
+    public String getSubject() {
+        return this.subject;
+    }
+
+    public ArrayList<Flashcard> getDeck() {
+        return this.deck;
+    }
+
+    public void setSubject(String name) {
+        this.subject = name;
+    }
+
+    public void addCard(Flashcard card) {
+        this.deck.add(card);
+    }
+
+    public void removeCard(Flashcard card) {
+        this.deck.remove(card);
+    }
 }
