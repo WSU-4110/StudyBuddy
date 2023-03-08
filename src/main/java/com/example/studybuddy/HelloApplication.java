d3f4a03e4a5743328387d12b34a2acd0dac7ee7c
DaeHwan Yoo <daehwanyoo0505@gmail.com>
2023-03-08 18:42:41 -0500

    Added: Scene for flashcard main page, edit page, add page and study page

--- src/main/java/com/example/studybuddy/HelloApplication.java
+++ src/main/java/com/example/studybuddy/HelloApplication.java
@@ -1,20 +1,14 @@
 package com.example.studybuddy;
 
-import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    private static Stage primaryStage;
-    public static Stage getPrimaryStage() {
-        return primaryStage;
-    }
     @Override
     public void start(Stage primaryStage) throws IOException {
         try {
