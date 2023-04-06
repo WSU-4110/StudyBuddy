0485e7014269e1250bae89d0c07e38bf1ddcc101
DaeHwan Yoo <daehwanyoo0505@gmail.com>
2023-04-06 17:45:22 -0400

    Updated: show the list of decks

--- src/main/java/com/example/studybuddy/SceneController.java
+++ src/main/java/com/example/studybuddy/SceneController.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class SceneController implements Initializable {
+public class SceneController {
     @FXML private Button mindmapsButton;
 
     @FXML private Button flashcardsButton;
@@ -23,9 +23,7 @@ public class SceneController implements Initializable {
     @FXML private AnchorPane rootPane;
     Parent parent;
     Scene scene;
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-    }
+
     public void switchScene(ActionEvent event, String fxmlFilename) {
         try {
             Parent mindmapsParent = FXMLLoader.load(getClass().getResource(fxmlFilename));
