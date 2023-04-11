208ffa54fa54c57cbec1c068ef584a301152cb78
Tom Charnley <he4834@wayne.edu>
2023-04-10 23:54:55 -0400

    unit testing

--- src/main/java/com/example/studybuddy/Map.java
+++ src/main/java/com/example/studybuddy/Map.java
@@ -2,35 +2,61 @@ package com.example.studybuddy;
 
 public class Map{
 
-    Node map[]=new Node[100];
-    public int i=1, max=100, s=1;
+    static Node map[]=new Node[100];
+
+    public static int i=1, max=100, s=1, setmulti=2;
 
     public Map(){ }
-    public Node getNode(int x){
+
+    public static Node getNode(int x){
         return map[x];
     }
 
-    public void addNode(double x, double y, String s){
+    public static Node addNode(double x, double y, String s){
         if(i>max){
-            extendo();
+            extend();
         }
         map[i]=new Node(x,y,s);
         i++;
+        return map[i-1];
     }
-    private void extendo()
+
+    public static int extend()
     {
-        max=max*2;
         Node map2[]=map;
+        max*=setmulti;
         map=new Node[max];
-        for(int a=0; a<max;a++){
+        for(int a=0; a<map2.length;a++){
             map[a]=map2[a];
         }
+        return max;
+    }
+    public static int changeMulti(int a){
+        setmulti=a;
+        return a;
     }
 
-    public Node getTop(){
-        return map[i];
+    public static int setMax(int a){
+        max=a;
+        return a;
     }
-    public void save(){
 
+    public static int setSelect(int a){
+        s=a;
+        return a;
+    }
+    public static boolean undoNode(){
+        s-=1;
+        if(s>=0){
+            return true;
+        }
+        return false;
+    }
+    public static boolean save(){
+        return true;
+    }
+
+    public static boolean load(){
+        return true;
     }
 }
