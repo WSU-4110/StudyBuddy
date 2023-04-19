654ca08d05bc93bcc9375694c4b1c85281976f95
adeel48180 <123700986+adeel48180@users.noreply.github.com>
2023-04-19 17:56:25 -0400

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.studybuddy {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.studybuddy to javafx.fxml;
-    exports com.example.studybuddy;
-}
