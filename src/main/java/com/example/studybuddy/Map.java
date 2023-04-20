16d980c05bb7da615eb361e5762d71d195f883bf
Tom Charnley <he4834@wayne.edu>
2023-04-20 16:36:52 -0400

    bug fixes and new buttons

--- src/main/java/com/example/studybuddy/Map.java
+++ src/main/java/com/example/studybuddy/Map.java
@@ -1,36 +1,64 @@
 package com.example.studybuddy;
 
-public class Map{
+import java.io.Serializable;
 
-    Node map[]=new Node[100];
-    public int i=1, max=100, s=1;
+public class Map implements Serializable {
+
+    static Node map[]=new Node[100];
+
+    public static int i=1, max=100, s=1, setmulti=2;
 
     public Map(){ }
-    public Node getNode(int x){
+
+    public static Node getNode(int x){
         return map[x];
     }
 
-    public void addNode(double x, double y, String s){
+    public static Node addNode(double x, double y, String s){
         if(i>max){
-            extendo();
+            extend();
         }
         map[i]=new Node(x,y,s);
         i++;
+        return map[i-1];
     }
-    private void extendo()
+
+    public static int extend()
     {
-        max=max*2;
         Node map2[]=map;
+        max*=setmulti;
         map=new Node[max];
-        for(int a=0; a<max;a++){
+        for(int a=0; a<map2.length;a++){
             map[a]=map2[a];
         }
+        return max;
+    }
+    public static int changeMulti(int a){
+        setmulti=a;
+        return a;
     }
 
-    public Node getTop(){
-        return map[i];
+    public static int setMax(int a){
+        max=a;
+        return a;
     }
-    public void save(){
 
+    public static int setSelect(int a){
+        s=a;
+        return a;
+    }
+    public static boolean undoNode(){
+        if(i>0){
+            i-=1;
+            return true;
+        }
+        return false;
+    }
+    public static boolean save(){
+        return true;
+    }
+
+    public static boolean load(){
+        return true;
     }
 }
--- src/main/java/com/example/studybuddy/Node.java
+++ src/main/java/com/example/studybuddy/Node.java
@@ -11,14 +11,20 @@ public class Node {
     public double x,y;
     public int bac;
     public Line l;
-    public static Image r=new Image("C:\\Users\\Tom Charnley\\rect10.png");
+    public static Image r=new Image("file:./src/main/resources/rect10.png");
     public Button B = new Button();
+    public TextField a;
     public String text;
 
     public Node(double xi, double yi, String s){
         x=xi; y=yi; text=s; //r.setX(x-r.getFitWidth()); r.setY(y-r.getFitHeight()); t.setText(s);
     }
     public Node (){
+    }
+    public void setText(){
+        a = new TextField(text);
+        a.setLayoutX(x);
+        a.setLayoutY(y);
     }
     public String toString(){
         return (x+" "+y+" "+l.getStartX()+" "+l.getStartY()+" "+l.getEndX()+" "+l.getEndY()+" "+bac+" "+text+"\n");
